3f77bd1e14a0bdfedb1768b949913fa4fc170951
Abhijith K <iamabhijith.k@gmail.com>
2021-10-04 20:08:54 +0530

    add models

--- msg_manager/views.py
+++ msg_manager/views.py
@@ -1,8 +1,8 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import View
-
+from .models import Message
 def hello_world(request):
-    print(request.method)
-    name = 'Shibin'
-    return render(request,'msg.html',{'key':name})
+    msgs=Message.objects.all()
+    name = request.user.username
+    return render(request,'msg.html',{'key':name,'msgs':msgs})
