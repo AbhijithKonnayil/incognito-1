9bd99eeb6ad6d647020180ba806d8137a9daab71
Abhijith K <iamabhijith.k@gmail.com>
2021-10-09 20:17:50 +0530

    auth check in views

--- msg_manager/urls.py
+++ msg_manager/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from msg_manager.views import MessageView,hello_world
 
 urlpatterns = [
-    path('',MessageView.as_view())
+    path('<username>/',MessageView.as_view())
 ]
 
--- msg_manager/views.py
+++ msg_manager/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from .models import Message
 from .forms import MessageForm
-
+from django.contrib.auth.models import Permission, User
 def hello_world(request):
     if(request.method=='GET'):
         #request.GET
@@ -21,16 +21,23 @@ def hello_world(request):
         return render(request,'msg.html',{'key':name,'form':form,'msgs':msgs,'rec':True})
 
 class MessageView(View):
-    def get(self,request):
-        form=MessageForm()
-        msgs=Message.objects.all()
-        name = request.user.username
-        return render(request,'msg.html',{'key':name,'form':form,'msgs':msgs})
     
-    def post(self,request):
+    def get(self,request,username):
+        if(request.user.is_authenticated):
+            form=MessageForm()
+            msgs=Message.objects.filter(user=request.user)
+            name = request.user.username
+            return render(request,'msg.html',{'key':name,'form':form,'msgs':msgs})
+        raise PermissionError('Permission Denied')
+    def post(self,request,username):
+        print(username)
         text=request.POST.get('text')
-        Message.objects.create(text=text)
+        user = User.objects.get(username=username)
+        Message.objects.create(text=text,user=user)
         form=MessageForm()
-        msgs=Message.objects.all()
+        msgs=Message.objects.filter(user=request.user)
         name = request.user.username
         return render(request,'msg.html',{'key':name,'form':form,'msgs':msgs,'rec':True})
+
+#MessageSend
+#
